90eb0ca916d25c74b9500ea8861135a3dcfdebd0
crauzx <minemonsterx@gmail.com>
2022-06-13 13:55:44 +0700

    update detail card style

--- src/components/Card/DetailCardView.jsx
+++ src/components/Card/DetailCardView.jsx
@@ -14,7 +14,7 @@ import Toast from "../Toast/Toast";
 
 const StyledCard = styled.div`
   display: flex;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
   padding: 5rem;
   border-radius: 1rem;
 
